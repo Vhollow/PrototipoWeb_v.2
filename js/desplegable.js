7064e8dec21f53e1d700b4f9b92da548dcfa1d37
Vicente Martínez <vicentemartinezf@gmail.com>
2016-05-10 21:18:41 +0200

    Primera version

---
+++ js/desplegable.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+
+
+
